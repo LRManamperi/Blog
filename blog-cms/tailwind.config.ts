fe3963c561bac2993ece3f3b7e87a51955ba649e
LaRasa99 <96112509+LaRasa99@users.noreply.github.com>
2024-02-18 16:10:00 +0530

    -final-project-

--- blog-cms/tailwind.config.ts
+++ blog-cms/tailwind.config.ts
@@ -6,15 +6,12 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
 };
 export default config;
